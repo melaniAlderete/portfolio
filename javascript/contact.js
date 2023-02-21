12db1ac64cc743fa0988ca64ae21fcc35661e04c
melani <alderetemelani11@gmail.com>
2023-02-21 19:47:18 -0300

    finished

---
+++ javascript/contact.js
@@ -0,0 +1,30 @@
+let sendButton = document.getElementById("submitButton");
+
+sendButton.onclick = sendMail;
+
+function sendMail() {
+  let params = {
+    from_name: document.getElementById("nombre").value,
+    email_id: document.getElementById("email").value,
+    message: document.getElementById("mensaje").value,
+  };
+  if (params.from_name != "" && params.email_id != "" && params.message != "") {
+    emailjs.send("service_aq7mtds", "template_hl2k34u", params).then((res) => {
+        Swal.fire({
+            icon: 'success',
+            title: 'Ya recibimos tu mensaje!',
+            showConfirmButton: false,
+            timer: 1600
+          });
+    }, (error)=>{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Algo salió mal!',
+            footer: 'Error: '+error.status,
+            showConfirmButton: false,
+            timer: 1600
+          })
+    });
+  }
+}
